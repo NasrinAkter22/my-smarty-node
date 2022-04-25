2383a33515cbd0874c4773f72d3f8328f9949ef6
NasrinAkter22 <258nasrinakter@gmail.com>
2022-04-25 22:50:52 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,36 @@
+/**  Basic sever set up
+ * .....................
+ *1.create a folder (manually or mkdir)
+ 2.open command link to that folder
+ 3. npm init
+ 4.npm install express
+ 5.create index.js
+ .................
+ .................
+
+ for express/node server
+ 1.requre express
+ 2.create app varriable
+ 3.declare port
+ 4.set app .get('/)
+ 5. listen to port 
+ 6.node index.js
+ 7.cheak your brwser for that port
+.................... 
+get
+1 cors middle ware
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
